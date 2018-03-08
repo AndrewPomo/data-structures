4c16dcf66b55536e81dc39b057d9407439015689
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-03-07 23:52:40 -0800

    Refactor pseudoclassical queue for time complexity

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,21 +2,25 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this.storage = {};
-  this.counter = 0;
+  this.high = 0;
+  this.low = 0;
 };
 
 Queue.prototype.size = function() {
-  return Object.keys(this.storage).length;
+  if (this.high <= this.low) {
+    return 0;
+  }
+  return this.high - this.low;
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.storage[this.counter] = value;
-  this.counter++;
+  this.high++;
+  this.storage[this.high] = value;
 };
 
 Queue.prototype.dequeue = function() {
-  var keys = Object.keys(this.storage);
-  var dequeueing = this.storage[keys[0]];
-  delete this.storage[keys[0]];
+  this.low++;
+  var dequeueing = this.storage[this.low];
+  delete dequeueing;
   return dequeueing;
 };
