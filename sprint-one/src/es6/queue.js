9409b58e77c69a4a1371bda789d13283f8d4b0a3
Andrew Powell-Morse <andrewpomo815@gmail.com>
2018-03-07 23:58:10 -0800

    Refactor es6 queue for time complexity

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,23 +2,27 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
-    this.counter = 0;
+    this.high = 0;
+    this.low = 0;
     this.storage = {};
   }
 
   size() {
-    return Object.keys(this.storage).length;
+    if (this.high <= this.low) {
+      return 0;
+    }
+    return this.high - this.low;
   }
 
   enqueue(value) {
-    this.counter++;
-    this.storage[this.counter] = value;
+    this.high++;
+    this.storage[this.high] = value;
   }
 
   dequeue() {
-    var keys = Object.keys(this.storage);
-    var dequeueing = this.storage[keys[0]];
-    delete this.storage[keys[0]];
+    this.low++;
+    var dequeueing = this.storage[this.low];
+    delete this.storage[this.low];
     return dequeueing;
   }
 }
